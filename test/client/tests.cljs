18428f660bc99487b41c9d36039079b2c69b64fb
maggot <mateuszwit21@gmail.com>
2017-03-07 08:49:48 +0100

    [test] Test mapping state to list of li elements

--- test/client/tests.cljs
+++ test/client/tests.cljs
@@ -1,7 +1,14 @@
 (ns client.tests
   (:require
+    [client.todo.core :refer [map-to-lis state]]
     [client.helpers :refer [greet-visitor]]
     [cljs.test :refer-macros [deftest is testing run-tests]]))
 
 (deftest greeting-visitor
   (is (= "Hello world!" (greet-visitor))))
+
+
+(deftest mapping-state-to-lis
+  (is (= '(
+            [:li "Learn ClojureScript"]
+            [:li "Write a great app in ClojureScript"]) (map-to-lis state))))
