bb876a1e7dc148e391884ec9b7118f6d9d330c95
maggot <mateuszwit21@gmail.com>
2017-03-07 22:33:27 +0100

    [test] Give isolated dummy state for tests

--- test/client/tests.cljs
+++ test/client/tests.cljs
@@ -1,12 +1,14 @@
 (ns client.tests
   (:require
-    [client.todo.core :refer [map-to-lis state]]
+    [client.todo.core :refer [map-to-lis]]
     [client.helpers :refer [greet-visitor]]
     [cljs.test :refer-macros [deftest is testing run-tests]]))
 
+(defonce state (atom [{:name "Learn ClojureScript" :is-done false}
+                      {:name "Write a great app in ClojureScript" :is-done false}]))
+
 (deftest greeting-visitor
   (is (= "Hello world!" (greet-visitor))))
-
 
 (deftest mapping-state-to-lis
   (is (= '(
