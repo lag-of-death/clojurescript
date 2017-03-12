19f7670bfc863886059e132128c808cf34c5ff2d
maggot <mateuszwit21@gmail.com>
2017-03-12 11:55:17 +0100

    [refactor] Use -> and ->> on the back-end side

--- src/server/core.cljs
+++ src/server/core.cljs
@@ -8,13 +8,29 @@
 (def todos (atom [{:name "Learn ClojureScript" :is-done false :id "0"}
                   {:name "Write a great app in ClojureScript" :is-done false :id "1"}]))
 
-(defn get-id [req] (get (js->clj (.-params req)) "id"))
+(defn get-id [req]
+  (-> (.-params req)
+      (js->clj)
+      (get "id")))
 
 (defn -main [& args]
   (let [app (express)]
+
     (.use app (.static express "resources/public"))
-    (.get app "/todos" (fn [req res] (.json res (clj->js @todos))))
-    (.delete app "/todos/:id" (fn [req res] (.json res (del-todo todos (get-id req)))))
+
+    (.get app "/todos"
+          (fn [req res]
+            (->> (clj->js @todos)
+                 (.json res))))
+
+
+    (.delete app "/todos/:id"
+             (fn [req res]
+               (->> req
+                    (get-id)
+                    (del-todo todos)
+                    (.json res))))
+
     (.listen app 4000)))
 
 (set! *main-cli-fn* -main)
