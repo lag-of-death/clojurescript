99fde067911f3e2e9e7df9c322cd7890d3070c11
maggot <mateuszwit21@gmail.com>
2017-03-12 17:13:56 +0100

    [refactor] Add function to create a single button

--- src/client/todo/core.cljs
+++ src/client/todo/core.cljs
@@ -13,9 +13,12 @@
       [res (<! (http/delete (str "http://localhost:4000/todos/" todo-id)))]
       (del-todo state (:body res)))))
 
-(def todo (fn [on-delete value]
-            ^{:key value} [:li (:name value)
-                           [:button {:on-click #(on-delete (:id value))} "x"]]))
+(defn button [on-delete] [:button {:on-click on-delete} "x"])
+
+(def todo
+  (fn [on-delete value]
+    (let [button (button (partial on-delete (:id value)))]
+      ^{:key value} [:li (:name value) button])))
 
 (defn map-to-lis [state on-delete]
   (let [on-del (partial on-delete state)]
