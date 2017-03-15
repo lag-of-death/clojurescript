56361370a71f59c8868ec9d72447e51315369dba
maggot <mateuszwit21@gmail.com>
2017-03-15 21:47:28 +0100

    [minor] Add todo only when name provided

--- src/client/todo/core.cljs
+++ src/client/todo/core.cljs
@@ -18,17 +18,11 @@
       [res (<! (http/delete (str "http://localhost:4000/todos/" todo-id)))]
       (del-todo state (:body res)))))
 
-(defn ask-server-to-add-todo [state todo-name]
+(defn on-add-btn-clicked [state todo-name]
   (go
     (let
       [res (<! (http/post "http://localhost:4000/todos" {:json-params {:todo-name todo-name}}))]
       (add-todo state (js->clj (:body res))))))
-
-(defn on-add-btn-clicked [state todo-name]
-  (if
-    (blank? todo-name)
-    (js/alert "Please define your TODO.")
-    (ask-server-to-add-todo state todo-name)))
 
 (defn on-input-change [input-atom evt] (swap! input-atom (fn [old-val] (-> evt .-target .-value))))
 
@@ -40,5 +34,5 @@
        [:div
         [:span "add todo:"]
         [:input {:on-change #(on-input-change input-atom %)}]
-        [button #(on-add-btn-clicked given-state @input-atom)
+        [:button {:disabled (blank? @input-atom) :on-click #(on-add-btn-clicked given-state @input-atom)}
          [:span "add"]]]])))
