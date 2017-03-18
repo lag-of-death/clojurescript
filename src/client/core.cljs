f12c2919e99ec8d9a71b63026e810880582449be
maggot <mateuszwit21@gmail.com>
2017-03-18 14:38:19 +0100

    [minor] Remove unnecessary imports

--- src/client/core.cljs
+++ src/client/core.cljs
@@ -2,9 +2,7 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require
     [client.state.changes :refer [react-on-changes]]
-    [client.state :refer [filter-todos-by input-atom todos]]
     [client.todo.core :as todo]
-    [cljs-http.client :as http]
     [reagent.core :as reagent]))
 
 (react-on-changes)
