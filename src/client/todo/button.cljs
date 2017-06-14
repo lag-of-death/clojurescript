4e3822c2bff9b71a678a6dd50806b5f9270fbea1
maggot <mateuszwit21@gmail.com>
2017-06-14 08:50:34 +0200

    [style] Add button class to button elements

--- src/client/todo/button.cljs
+++ src/client/todo/button.cljs
@@ -7,14 +7,15 @@
 
 (defn handle-del-btn-clicked-with-channel [channel todo-id]
   (go
-    (let
-      [res (<! (http/delete (str "http://localhost:4000/todos/" todo-id)))]
-      (put! channel res))))
+   (let [res (<! (http/delete (str "http://localhost:4000/todos/" todo-id)))]
+     (put! channel res))))
 
-(def handle-del-btn-clicked (partial handle-del-btn-clicked-with-channel del-todo-channel))
+(def handle-del-btn-clicked
+  (partial handle-del-btn-clicked-with-channel del-todo-channel))
 
-(defn del-button [on-click todo-id] [:button {:on-click #(on-click todo-id)} "x"])
+(defn del-button [on-click todo-id]
+  [:button.button {:on-click #(on-click todo-id)} "x"])
 
 (defn button
   ([todo-id] (del-button handle-del-btn-clicked todo-id))
-  ([on-click child-node] [:button {:on-click on-click} child-node]))
+  ([on-click child-node] [:button.button {:on-click on-click} child-node]))
